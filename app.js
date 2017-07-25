295989a845848db0a57fb4ce88c41b0c5d685861
jnprogrammer <joshuanparker@gmail.com>
2017-07-24 22:35:25 -0400

    Finished V10

--- app.js
+++ app.js
@@ -58,10 +58,8 @@ var handlers = {
     changeTodoTextInput.value ='';
     view.displayTodos();
   },
-  deleteTodo:function(){
-    var deleteTodoPositionInput = document.getElementById('deleteTodoPositionInput');
-    todoList.deleteTodo(deleteTodoPositionInput.valueAsNumber);
-    deleteTodoPositionInput.value = '';
+  deleteTodo:function(pos){
+    todoList.deleteTodo(pos);
     view.displayTodos();
   },
   toggleCompleted: function(){
@@ -92,9 +90,48 @@ var view = {
         todoTextWithCompletion = '( ) ' + 
         todo.todoText;
       }
-
+      todoLi.id = i;
       todoLi.textContent = todoTextWithCompletion;
+      todoLi.appendChild(this.createDeleteButton());
       todosUl.appendChild(todoLi);
       }
+  },
+  createDeleteButton: function(){
+    var deleteButton = document.createElement('button');
+    deleteButton.textContent = 'Delete';
+    deleteButton.className = 'deleteButton';
+
+
+    return deleteButton;
+  },
+  setUpEventListeners: function(){
+   var todosUl = document.querySelector('ul');
+   todosUl.addEventListener('click',function(event){
+
+    // Get the element that was clicked on.
+    var elementClicked = event.target;
+
+    //Check if elementClicked is a delete button
+    if(elementClicked.className ==='deleteButton'){
+      handlers.deleteTodo(parseInt(elementClicked.parentNode.id));
+      
+    }
+   });
   }
 };
+
+view.setUpEventListeners();
+
+/*  
+For Debugging in console
+
+function runDebugger(myFunction){
+  debugger;
+  myFunction();
+}
+
+For timing function calls
+SetTimout(function(){},5000){
+  console.log("Running Function");
+}
+*/
